c42d2a7e3f8cce4e8c11fc3b359225e21cbc2438
Paul Choquette <pchoquette@gmail.com>
2016-06-21 10:54:16 -0400

    Added O365BulkInheritanceReset.ps1

---
+++ O365BulkInheritanceReset.ps1
@@ -0,0 +1,49 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$true, HelpMessage="This is the URL to the SharePoint Online site with the document library where you want to reset inheritance.")]
+    [string]$Url, 
+
+    [Parameter(Mandatory=$true, HelpMessage="This is the name of the document library where you want to reset inheritance.")]
+    [string]$ListName, 
+
+    [Parameter(Mandatory=$false, HelpMessage="This is the path to the DLLs for CSOM.")]
+    [string]$CSOMPath
+)
+
+Set-Strictmode -Version 1
+
+If ($CSOMPath -eq $null -or $CSOMPath -eq "") { $CSOMPath = "." }
+
+Add-Type -Path "$CSOMPath\Microsoft.SharePoint.Client.dll" 
+Add-Type -Path "$CSOMPath\Microsoft.SharePoint.Client.Runtime.dll" 
+
+$psCredentials = Get-Credential
+$spoCredentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($psCredentials.UserName, $psCredentials.Password)
+$clientContext = New-Object Microsoft.SharePoint.Client.ClientContext($Url) 
+$clientContext.Credentials = $spoCredentials 
+
+$domain = ([System.Uri]$Url).Host
+$userName = $psCredentials.UserName
+
+If ($clientContext.ServerObjectIsNull.Value) { 
+    Write-Error "Could not connect to SharePoint Online site collection: $Url"
+} Else {
+    Write-Host "Connected to SharePoint Online site collection: " $Url -ForegroundColor Green        
+                
+    $web = $clientContext.Web
+    $clientContext.Load($web)
+    $list = $web.Lists.GetByTitle($ListName)
+    $clientContext.Load($list)
+    $clientContext.ExecuteQuery()
+
+    $query = [Microsoft.SharePoint.Client.CamlQuery]::CreateAllItemsQuery()
+    $items = $list.GetItems($query)
+    $clientContext.Load($items)
+    $clientContext.ExecuteQuery()
+
+    $items | % {
+        $_.ResetRoleInheritance()
+        $_.Update()
+    }
+    $clientContext.ExecuteQuery()
+}
